5890e107cd6e0f1057f6edb003ad552b158d81af
programmeter <martin@tuckdesign.com>
2025-05-29 17:50:09 +0200

    Added g++ alias in MacOS build script

--- build/build-install-mac.sh
+++ build/build-install-mac.sh
@@ -14,7 +14,8 @@ mkdir -p /usr/local/include/sdl2-gdk
 cp -r include/* /usr/local/include/sdl2-gdk/
 
 # Change this to the location of the g++ binary on your system
-/usr/local/Cellar/gcc/15.1.0/bin/g++-15 -dynamiclib -I/usr/local/include/ -fPIC -O2 -o libgdk.dylib src/*.cpp -framework OpenGL -lSDL2 -lSDL2_image -lSDL2_ttf -lSDL2_mixer
+alias g++=/usr/local/Cellar/gcc/15.1.0/bin/g++-15
+g++ -dynamiclib -I/usr/local/include/ -fPIC -O2 -o libgdk.dylib src/*.cpp -framework OpenGL -lSDL2 -lSDL2_image -lSDL2_ttf -lSDL2_mixer
 
 mv libgdk.dylib /usr/local/lib/
 
